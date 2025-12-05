e71f57e6624a3d0edb8856c691cc2b5e8f9d7156
Mudit Krishna Shrivastava <muditshrivastav47@gmail.com>
2025-12-06 01:55:35 +0530

    day 1 progress

---
+++ day1_basics/input_output.py
@@ -0,0 +1,2 @@
+x = int(input("Enter a number: "))
+print("you will be", x +10 ,"years old in 10yrs")
---
+++ day1_basics/multiline_string.py
@@ -0,0 +1,7 @@
+print('''Hello 
+this is a multi line string 
+yoooooooo''')
+
+# multi line strings using (newline character) or triple quotes(''')
+
+print("\nhello \nthis is also a multi line string \nusing newline character \nyoooooooo")
